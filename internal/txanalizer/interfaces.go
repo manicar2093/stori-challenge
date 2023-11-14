55f39f4c00748387e1300b806e12d4554dde612b
Manuel Carbajal <manuel@montebit.io>
2023-11-14 17:08:45 -0600

    chore: rename email interface

--- internal/txanalizer/interfaces.go
+++ internal/txanalizer/interfaces.go
@@ -1,15 +1,20 @@
 package txanalizer
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/jordan-wright/email"
+)
 
 type (
 	TransactionRepository interface {
 		Create(input CreateAccountTransactionsInput) error
 	}
 
-	EmailSender interface {
-		SendAccountDetailsEmail(input SendAccountDetailsEmailInput) (uuid.UUID, error)
+	Notificator interface {
+		SendAccountDetailsEmail(input SendAccountDetailsEmailInput) error
 	}
+
+	EmailSender func(*email.Email, MailgunConfig) error
 
 	UUIDCreator func() uuid.UUID
 )
